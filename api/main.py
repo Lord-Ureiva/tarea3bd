0c39c6e381bbdace281045b9187c835b39d3484f
Juan Mira <juan.mirag@usm.cl>
2021-07-02 23:48:23 -0400

    changes

---
+++ api/main.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask import jsonify
+from config import config
+from flask import request
+
+def create_app(enviroment):
+	app = Flask(__name__)
+	app.config.from_object(enviroment)
+	with app.app_context():
+		db.init_app(app)
+		db.create_all()
+	return app
+
+# Accedemos a la clase config del archivo config.py
+enviroment = config['development']
+app = create_app(enviroment)
